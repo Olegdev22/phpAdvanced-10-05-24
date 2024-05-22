181816d68eeea22df7da48b253558bb51e16f7a3
Olegdev22 <kirbaba2006@ukr.net>
2024-05-22 19:31:50 +0300

    заготовка к ovrloding

--- index.php
+++ index.php
@@ -1,2 +1,67 @@
 <?php
     require_once "vendor/autoload.php";
+    class User
+{
+    private string $name;
+    private int $age;
+    private string $email;
+
+    /*  public function __construct($name, $age, $email)
+      {
+          $this->name = $name;
+          $this->age = $age;
+          $this->email = $email;
+      }*/
+
+
+    private function getName(): string
+    {
+        return $this->name;
+    }
+
+    private function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+
+    private function getAge(): int
+    {
+        return $this->age;
+    }
+
+    private function setAge($age): void
+    {
+        $this->age = $age;
+    }
+
+
+    private function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    private function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getAll(): void
+    {
+
+    }
+
+    public function __call($name, $arguments)
+    {
+        echo "Call to undefined method $name";
+    }
+}
+
+$user = new User;
+/*$user->setName('Alex');
+$user->setAge(20);
+$user->setEmail('example@gmail.com');*/
+
+$user->test1Method('something');
+$user->getAll();
+print_r($user);
