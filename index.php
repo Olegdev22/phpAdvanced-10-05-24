6c23550dd4e80cc38e94f05b4b69929621af0d75
Olegdev22 <kirbaba2006@ukr.net>
2024-05-25 16:27:02 +0300

    dockerfile+pdo

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-    require_once "vendor/autoload.php";
+require_once  "vendor/autoload.php";
+
+$dsn = "mysql:host=database;dbname=phpPro_10.05.24";
+
+try {
+    $pdo = new PDO($dsn, 'root', '123456');
+} catch (PDOException $e) {
+    dd($e);
+}
