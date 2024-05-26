9cfa035563ae4d27abf06070f7c35ead9a8b3613
Olegdev22 <example@gmail.com>
2024-05-26 23:10:13 +0300

    dockerfile+pdo update

--- phpAdvanced-10-05-24/index.php
+++ phpAdvanced-10-05-24/index.php
@@ -1,10 +1,12 @@
 <?php
-require_once  "vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 echo "hello";
-$dsn = "mysql:host=database;dbname=phpPro_10.05.24";
+
+$dsn = "mysql:host=database;database=phpPro_10.05.24";
 
 try {
     $pdo = new PDO($dsn, 'root', '123456');
+    dd($pdo);
 } catch (PDOException $exception) {
     dd($exception);
 }
