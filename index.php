ccdba846b82193b6c33897eab3977b913505c607
Olegdev22 <kirbaba2006@ukr.net>
2024-06-22 17:33:21 +0300

    create solid branche

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
-echo '139999990';
-
-$dsn = "mysql:host=database;database=phpPro_10.05.24";
-
-try {
-    $pdo = new PDO($dsn, 'root', '123456');
-    dd($pdo);
-} catch (PDOException $exception) {
-    dd($exception);
-}
