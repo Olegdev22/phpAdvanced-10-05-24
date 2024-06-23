ea9d25da515d80cfbb2fbd50851423f943d42602
Olegdev22 <kirbaba2006@ukr.net>
2024-06-23 12:12:39 +0300

    interface segregation

--- index.php
+++ index.php
@@ -1,2 +1,48 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
+
+//interface Bird
+//{
+//    public function eat();
+//    public function fly();
+//}
+//
+//class Swallow implements Bird
+//{
+//    public function eat() {}
+//    public function fly() {}
+//}
+//
+//class Ostrich implements Bird
+//{
+//    public function eat() {}
+//    public function fly() { /* exception */ }
+//}
+
+interface BirdEat
+{
+    public function eat();
+}
+
+interface BirdFly
+{
+    public function fly();
+}
+
+class Swallow implements BirdEat
+{
+    public function eat()
+    {
+    }
+
+    public function fly()
+    {
+    }
+}
+
+class Ostrich implements BirdEat
+{
+    public function eat()
+    {
+    }
+}
